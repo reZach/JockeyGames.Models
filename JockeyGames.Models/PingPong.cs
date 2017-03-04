d6aa7f1c3f93f187acd90cf8b1ed0476fefe992c
Zachary <zwenta@carthage.edu>
2017-03-03 20:00:00 -0600

    added models

---
+++ JockeyGames.Models/PingPong.cs
@@ -0,0 +1,45 @@
+﻿using JockeyGames.Models.Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JockeyGames.Models.PingPong
+{
+    public class PlayerGame
+    {
+        public int Id { get; set; }
+        public int Score { get; set; }
+
+        public virtual Player PlayerId { get; set; }
+    }
+
+    public class Game
+    {
+        public int Id { get; set; }
+
+        public virtual PlayerGame PlayerGameId1 { get; set; }
+        public virtual PlayerGame PlayerGameId2 { get; set; }
+    }
+
+    public class Match
+    {
+        public int Id { get; set; }
+        [Column(TypeName = "datetime2")]
+        public DateTime DateTime { get; set; }
+
+        public virtual Player PlayerId1 { get; set; }
+        public virtual Player PlayerId2 { get; set; }
+        public ICollection<Game> Games { get; set; }
+    }
+
+    public class Tournament
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }    
+            
+        public virtual ICollection<Match> Matches { get; set; }
+    }
+}

1b5818e835db518d2304a64ef61a0f5d88c852c1
Zachary <zwenta@carthage.edu>
2017-03-04 21:41:31 -0600

    moving towards a simpler data model

--- JockeyGames.Models/DTOs/PingPongDTO.cs
+++ JockeyGames.Models/DTOs/PingPongDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JockeyGames.Models.PingPong;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -7,32 +8,25 @@ using System.Threading.Tasks;
 
 namespace JockeyGames.Models.DTOs
 {
-    public class PlayerGameDTO
+    public class PlayerGameDTO : Player
     {
-        public int Score { get; set; }
-        public PlayerDTO Player { get; set; }
-    }
-
-    public class GameDTO
-    {
-        public int Id { get; set; }
-        public MatchDTO Match { get; set; }
-        public ICollection<PlayerGameDTO> PlayerGames { get; set; }
+                
     }
 
     public class MatchDTO
     {
         public int Id { get; set; }
+
         [Column(TypeName = "datetime2")]
         public DateTime DateTime { get; set; }
-        public TournamentDTO Tournament { get; set; }
-        public ICollection<GameDTO> Games { get; set; }
-    }
+        public int G1P1Score { get; set; }
+        public int G1P2Score { get; set; }
+        public int G2P1Score { get; set; }
+        public int G2P2Score { get; set; }
+        public int G3P1Score { get; set; }
+        public int G3P2Score { get; set; }
 
-    public class TournamentDTO
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public ICollection<MatchDTO> Matches { get; set; }
+        public Player Player1 { get; set; }
+        public Player Player2 { get; set; }
     }
 }
--- JockeyGames.Models/PingPong.cs
+++ JockeyGames.Models/PingPong.cs
@@ -1,55 +1,28 @@
-﻿using JockeyGames.Models.Shared;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace JockeyGames.Models.PingPong
 {
-    public class PlayerGame
-    {
-        // Foreign keys
-        [Key, Column(Order = 0)]
-        public int GameId { get; set; }
-        [Key, Column(Order = 1)]
-        public int PlayerId { get; set; }
-
-        // Navigation properties
-        [Required]
-        public virtual Game Game { get; set; }
-        [Required]
-        public virtual Player Player { get; set; }
-
-        // Additional properties
-        public int Score { get; set; }
-    }
-
-    public class Game
+    public class Player
     {
         public int Id { get; set; }
-
-        public virtual Match Match { get; set; }
-        public virtual ICollection<PlayerGame> PlayerGames { get; set; }
+        public string Name { get; set; }
     }
 
     public class Match
     {
         public int Id { get; set; }
+
         [Column(TypeName = "datetime2")]
         public DateTime DateTime { get; set; }
+        public int G1P1Score { get; set; }
+        public int G1P2Score { get; set; }
+        public int G2P1Score { get; set; }
+        public int G2P2Score { get; set; }
+        public int G3P1Score { get; set; }
+        public int G3P2Score { get; set; }
 
-        public virtual Tournament Tournament { get; set; }
-        public virtual ICollection<Game> Games { get; set; }
-    }
-
-    public class Tournament
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }    
-            
-        public virtual ICollection<Match> Matches { get; set; }
+        public virtual Player PlayerId1 { get; set; }
+        public virtual Player PlayerId2 { get; set; }
     }
 }

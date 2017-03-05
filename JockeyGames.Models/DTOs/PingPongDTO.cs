e1e16bda4eaf1af57ba0f734d20ed09777a95f2f
Zachary <zwenta@carthage.edu>
2017-03-05 00:10:47 -0600

    made things simpler

--- JockeyGames.Models/DTOs/PingPongDTO.cs
+++ JockeyGames.Models/DTOs/PingPongDTO.cs
@@ -1,6 +1,7 @@
 ﻿using JockeyGames.Models.PingPong;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,10 @@ using System.Threading.Tasks;
 
 namespace JockeyGames.Models.DTOs
 {
-    public class PlayerDTO : Player
+    public class PlayerDTO
     {
-                
+        public int Id { get; set; }            
+        public string Name { get; set; }
     }
 
     public class MatchDTO
@@ -19,14 +21,26 @@ namespace JockeyGames.Models.DTOs
 
         [Column(TypeName = "datetime2")]
         public DateTime DateTime { get; set; }
+        [Display(Name = "Game 1 | P1")]
+        [Range(0, 21)]
         public int G1P1Score { get; set; }
+        [Display(Name = "Game 1 | P2")]
+        [Range(0, 21)]
         public int G1P2Score { get; set; }
+        [Display(Name = "Game 2 | P1")]
+        [Range(0, 21)]
         public int G2P1Score { get; set; }
+        [Display(Name = "Game 2 | P2")]
+        [Range(0, 21)]
         public int G2P2Score { get; set; }
+        [Display(Name = "Game 3 | P1")]
+        [Range(0, 21)]
         public int G3P1Score { get; set; }
+        [Display(Name = "Game 3 | P2")]
+        [Range(0, 21)]
         public int G3P2Score { get; set; }
 
-        public Player Player1 { get; set; }
-        public Player Player2 { get; set; }
+        public int PlayerId1 { get; set; }
+        public int PlayerId2 { get; set; }
     }
 }
